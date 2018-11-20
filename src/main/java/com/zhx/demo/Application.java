8b34ef1483683c1ad9215f3a24d18322a5f24f5e
kangxiongwei <kangxiongwei@163.com>
2018-11-20 22:59:42 +0800

    新增UDP接受数据的逻辑

--- src/main/java/com/zhx/demo/Application.java
+++ src/main/java/com/zhx/demo/Application.java
@@ -1,10 +1,15 @@
 package com.zhx.demo;
 
+import com.zhx.demo.service.IDeviceService;
+import com.zhx.demo.service.impl.DeviceService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * 启动服务的类
@@ -15,8 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("com.zhx.demo.dao")
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        IDeviceService deviceService = context.getBean("deviceService", DeviceService.class);
+        deviceService.start(8899);
     }
 
     @RequestMapping("/hello")
---
+++ src/main/java/com/zhx/demo/service/impl/DeviceService.java
@@ -0,0 +1,52 @@
+package com.zhx.demo.service.impl;
+
+import com.zhx.demo.service.IDeviceService;
+import com.zhx.demo.service.handler.EchoSeverHandler;
+import org.apache.mina.filter.executor.ExecutorFilter;
+import org.apache.mina.transport.socket.DatagramSessionConfig;
+import org.apache.mina.transport.socket.nio.NioDatagramAcceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+/**
+ * Create by kangxiongwei on 2018/11/20 下午10:30
+ */
+@Service
+public class DeviceService implements IDeviceService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeviceService.class);
+
+    @Override
+    public void start(int port) throws IOException {
+        // ** Acceptor设置
+        NioDatagramAcceptor acceptor = new NioDatagramAcceptor();
+        // 此行代码能让你的程序整体性能提升10倍 建立线程池
+        acceptor.getFilterChain()
+                .addLast("threadPool", new ExecutorFilter(Executors.newCachedThreadPool()));
+        // 设置MINA2的IoHandler实现类
+        acceptor.setHandler(new EchoSeverHandler());
+        // 设置会话超时时间（单位：毫秒），不设置则默认是10秒，请按需设置
+        //acceptor.setSessionRecycler(new ExpiringSessionRecycler(15 * 1000));
+
+        // ** UDP通信配置
+        DatagramSessionConfig dcfg = acceptor.getSessionConfig();
+        // 设置每一个非主监听连接端口可以重用
+        dcfg.setReuseAddress(true);
+        // 设置输入缓冲区的大小，压力测试表明：调整到2048后性能反而降低
+        dcfg.setReceiveBufferSize(1024);
+        // 设置输出缓冲区的大小，压力测试表明：调整到2048后性能反而降低
+        dcfg.setSendBufferSize(1024);
+
+        // ** UDP服务端开始侦听
+        acceptor.bind(new InetSocketAddress(port));
+
+        LOGGER.info("[IMCORE]UDP服务器正在端口 8899 上监听中...");
+    }
+
+
+}
